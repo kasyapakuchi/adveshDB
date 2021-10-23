f849c6b5229f3576658832aa8415459f250e9336
kasyapakuchi <kasyapa.kvsm@gmail.com>
2021-10-23 16:37:44 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,3 +1,21 @@
 #product
 
 INSERT INTO `advesh`.`product` (`ProductID`, `shortName`, `longName`, `imageUrls`, `price`, `description`, `category`, `subCategory`, `material`, `colour`) VALUES ('10001', 'Red Saree', 'Red Saree', 'http://cdn.shopify.com/s/files/1/2202/3303/products/image_717a090b-c6fa-4570-a3cd-c199cd1fd642_1024x1024@2x.jpg?v=1595425173\"', '100', 'Red colour Saree', 'Sarees', 'Fancy Sarees', 'Silk', 'Red');
+
+
+#admin
+INSERT INTO `advesh`.`seller`
+(`username`,
+`password`,
+`fname`,
+`lname`,
+`mobile`,
+`email`)
+VALUES
+('cpadadmin',
+'',
+'Kasyapa',
+'Kuchi',
+'8686919615',
+'cpadbits@gmail.com');
+
