dbfcc14605603a17d24775c4da8b932493765063
kasyapakuchi <kasyapa.kvsm@gmail.com>
2021-10-24 18:21:40 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,6 +2,15 @@
 
 INSERT INTO `advesh`.`product` (`ProductID`, `shortName`, `longName`, `imageUrls`, `price`, `description`, `category`, `subCategory`, `material`, `colour`) VALUES ('10001', 'Red Saree', 'Red Saree', 'http://cdn.shopify.com/s/files/1/2202/3303/products/image_717a090b-c6fa-4570-a3cd-c199cd1fd642_1024x1024@2x.jpg?v=1595425173\"', '100', 'Red colour Saree', 'Sarees', 'Fancy Sarees', 'Silk', 'Red');
 
+INSERT INTO `advesh`.`product` (`ProductID`, `shortName`, `longName`, `imageUrls`, `price`, `description`, `category`, `subCategory`, `material`, `colour`) VALUES ('13010', 'Lehenga', 'Red Bridal Lehenga', 'https://cdn.shopify.com/s/files/1/2202/3303/products/image_717a090b-c6fa-4570-a3cd-c199cd1fd642_1024x1024@2x.jpg?v=1595425173\%22', '10000', 'Traditional Bridal Lehenga for Weddings', 'Lehenga', 'Bridal', 'Georgette', 'Red');
+
+INSERT INTO `advesh`.`product` (`ProductID`, `shortName`, `longName`, `imageUrls`, `price`, `description`, `category`, `subCategory`, `material`, `colour`) VALUES ('15134', 'Dress Material', 'Pink Neck and Border Dress Material', 'https://cdn.shopify.com/s/files/1/2395/9105/products/WhatsAppImage2020-07-23at10.18.23PMg_800x998.jpg?v=1610620132', '1530', 'Casual Neck and Border Dress Material for daily wear', 'Dress', 'Casual', 'Georgette', 'Pink');
+
+INSERT INTO `advesh`.`product` (`ProductID`, `shortName`, `longName`, `imageUrls`, `price`, `description`, `category`, `subCategory`, `material`, `colour`) VALUES ('19826', 'Saree', 'Brasso Saree', 'https://static.peachmode.com/media/images/product/104633/base/1623266282_1_SUBAS-VIRASAT-31903_-PEACHMODE.jpg', '4049', 'Desirable Blue Coloured Partywear Printed Brasso Saree', 'Sarees', 'Party Wear', 'Satin', 'Blue');
+
+INSERT INTO `advesh`.`product` (`ProductID`, `shortName`, `longName`, `imageUrls`, `price`, `description`, `category`, `subCategory`, `material`, `colour`) VALUES ('25678', 'Saree', 'Bridal Saree', 'https://www.weddingwishlist.com/wedding-board/wp-content/uploads/2019/11/Bridal-Saree.png', '8675', 'Desirable Blue Coloured Partywear Printed Brasso Saree', 'Sarees', 'Bridal', 'Silk', 'Red and Green');
+
+INSERT INTO `advesh`.`product` (`ProductID`, `shortName`, `longName`, `imageUrls`, `price`, `description`, `category`, `subCategory`, `material`, `colour`) VALUES ('17658', 'Saree', 'Silk Saree', 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202109/yamisar.jpg?CO_0ZjR520VjxhAB9O9qJv8IZUUpbV6G&size=770:433', '28000', 'Zari Silk Saree', 'Sarees', 'Party Wear', 'Silk', 'Pink');
 
 #admin
 INSERT INTO `advesh`.`seller`
